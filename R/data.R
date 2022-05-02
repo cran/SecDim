96a65b15536c86f81c0e3fcc0c929e0ea0e7efe1
Yongze Song <yongze.song@postgrad.curtin.edu.au>
2022-05-02 10:30:12 +0000

    version 1.3

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' @title Spatial datasets of trace elements.
+#'
+#' @description Spatial datasets of trace elements.
+#'
+#' @name obs
+#' @format \code{obs}: A data frame of trace elements with 614 rows and 5 variables
+#' @docType data
+#' @author Yongze Song \email{yongze.song@curtin.edu.au}
+#' @keywords dataset trace elements
+"obs"
+
+
+#' @title Spatial datasets of the second dimension variables of trace elements.
+#'
+#' @description Spatial datasets of the second dimension variables of trace elements.
+#'
+#' @name sample_vars_sda
+#' @format \code{sample_vars_sda}: A list with 8 elements
+#' @docType data
+#' @author Yongze Song \email{yongze.song@curtin.edu.au}
+#' @keywords dataset trace elements factors
+"sample_vars_sda"
